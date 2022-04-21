57e77456a16dbb8d1460042b218bd7e81d72995b
Graham Otte <go@grahamotte.com>
2022-04-20 18:12:08 -0700

    Cleanup

--- lib/qprof.rb
+++ lib/qprof.rb
@@ -16,20 +16,23 @@ module QProf
       run_svg_path = "/tmp/qprof/#{run}.svg"
       flamegraph_pl_path = '/tmp/qprof/FlameGraph/flamegraph.pl'
 
+      # pull flamegraph if it's needed
       `git clone https://github.com/brendangregg/FlameGraph.git #{File.dirname(flamegraph_pl_path)}` unless File.exist?(flamegraph_pl_path)
 
+      # run block
       @benchmark = Benchmark.measure do
         @result = RubyProf.profile do
           yield
         end
       end
 
-      subtitle = %i[utime stime real]
-        .map { |x| "#{x} = #{@benchmark.send(x).round(4)}s" }
-        .join('; ')
+      # generate graph
+      subtitle = %i[utime stime real].map { |x| "#{x} = #{@benchmark.send(x).round(4)}s" }.join('; ')
       printer = RubyProf::FlameGraphPrinter.new(@result)
       File.open(run_txt_path, 'w') { |file| printer.print(file) }
       `cat #{run_txt_path} | #{flamegraph_pl_path} --title \"#{title}\" --subtitle \"#{subtitle}\" > #{run_svg_path}`
+
+      # open in browser
       Launchy.open(run_svg_path)
     end
   end
